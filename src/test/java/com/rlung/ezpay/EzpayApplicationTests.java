1d534fa8bafbc50d154ad70cb5b26515c406167c
unknown <rlungswe@gmail.com>
2025-11-22 22:17:28 +1100

    initial Spring Boot project setup

---
+++ src/test/java/com/rlung/ezpay/EzpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlung.ezpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
